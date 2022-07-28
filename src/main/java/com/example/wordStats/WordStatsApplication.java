979574c873183aa26f4ea317ef1a3b7a23f306e4
h4msterilla <97986397+h4msterilla@users.noreply.github.com>
2022-07-28 13:59:22 +0300

    upload spring project

---
+++ src/main/java/com/example/wordStats/WordStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wordStats/WordStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
